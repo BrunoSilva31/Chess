6f81f8e0c8351a0fbe7ead9cb1bb7aadf98b9e83
Bruno Silva <brunosilva9725@gmail.com>
2024-06-03 19:05:57 -0300

    Starting to implement Board and Piece

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,11 @@
 package src.app;
 
+import src.Board.Board;
 import src.Board.Position;
 
 public class Program {
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-
-        System.out.println(pos);
+       Board board = new Board(8, 8);
     }
 }
